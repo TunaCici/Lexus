317f2e0fec4161c122c590c51c62a995b8ff199d
Tuna Cici <tunacici7@gmail.com>
2021-08-20 21:25:11 +0300

    Added the new filesystem.

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Made by Project Lexus Team
+Name: main.py
+Purpose: Controls each module
+and provides logic for the progam.
+
+Author: Tuna Cici
+Created: 19/08/2021
+"""
+
+
+from modules import logger
+
+# entry point
+if __name__ == "__main__":
+    yazici = logger.LexusLogger()
+
+    yazici.log_info("Hey bir sey ters gitti.")
---
+++ modules/tester.py
@@ -0,0 +1,20 @@
+"""
+Name: tester.py
+Purpose: provide test functions and values
+for the controller.
+
+Author: Team Crossbones
+Created: 20/08/2021
+"""
+
+import logger
+
+def test_logger():
+    test_logger = logger.LexusLogger()
+    test_logger.log_info("Hello from the tester file.")
+    test_logger.log_warning("This is a awrning. Careful.")
+    test_logger.log_error("Something went wrong. Error!")
+    print("You should not see this.")
+
+if __name__ == "__main__":
+    test_logger()
