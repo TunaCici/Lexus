e50dadad755cb214a0e241896261f5d5aa3f1f0d
Tuna Cici <tunacici7@gmail.com>
2021-08-22 01:00:10 +0300

    Added configuration file for the project.

---
+++ config/lexus_config.py
@@ -0,0 +1,20 @@
+"""
+Made by Project Lexus Team
+Name: lexus_config.py
+Purpose: Includes setting variables for the project.
+
+Author: Tuna Cici
+Created: 22/08/2021
+"""
+
+import os
+
+# Project
+PROJECT_DIR = os.getcwd()
+
+# Controller
+
+# Debug_screen
+
+# Logger
+
