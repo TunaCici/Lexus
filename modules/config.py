0025f3e0e1b0942afb765483e9bf8f34f0914480
Dilara <n.dilara.oezcan@outlook.de>
2021-08-22 17:21:13 +0300

    config dosyasindakki ekledigim degisken THRESHOLD

--- modules/config.py
+++ modules/config.py
@@ -19,3 +19,6 @@ PROJECT_DIR = os.getcwd()
 # Logger
 LOG_FORMAT = "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"
 LOG_FILE_DIR = PROJECT_DIR + "/logs/lexuslogfile.txt"
+
+#Ultrasonic_Sensor
+THRESHOLD = 2000 #mm
