a30e3a21cf149fe987689e14ee367263a2a20547
Tuna Cici <tunacici7@gmail.com>
2021-08-27 00:58:30 +0300

    Coding for AI is done. Need to wrap it all later.

--- modules/ai.py
+++ modules/ai.py
@@ -9,6 +9,10 @@ Created: 25/08/2021
 
 import os
 import random
+import glob
+import cv2
+import numpy
+import time
 
 from ctypes import *
 
@@ -174,7 +178,7 @@ def detect_image(
     with highest confidence class and their bounding box
     """
 
-    pnum = POINTER(c_int(0))
+    pnum = pointer(c_int(0))
     predict_image(network, image)
     detections = get_network_boxes(
         network,
@@ -338,6 +342,173 @@ network_predict_batch.restype = POINTER(DetNumPair)
 
 # ----------------------------------------------------------------------------
 
+input_file = ""
+batch_size = 1
+weights = "yolov4.weights"
+config_file = "cfg/yolov4.cfg"
+data_file = "cfg/coco.data"
+thresh = 0.25
+
+def check_batch_shape(images, batch_size):
+    """
+    all the images must have the same width and height
+    """
+    shapes = [image.shape for image in images]
+    if len(set(shapes)) > 1:
+        raise ValueError("Images do not have the same shape")
+    if len(shapes) > batch_size:
+        raise ValueError("Batch size is smaller than number of images")
+    
+    return shapes[0]
+
+def load_images(images_path):
+    """
+    If image path is given, return it directly
+    For txt file, read it and return each line as image path
+    In other case, it's a folder, return a list with names of each
+    jpg, jpeg and png file
+    """
+    input_path_extension = images_path.split('.')[-1]
+    if input_path_extension in ['jpg', 'jpeg', 'png']:
+        return [images_path]
+    elif input_path_extension == "txt":
+        with open(images_path, "r") as f:
+            return f.read().splitlines()
+    else:
+        return glob.glob(
+            os.path.join(images_path, "*.jpg")) + \
+            glob.glob(os.path.join(images_path, "*.png")) + \
+            glob.glob(os.path.join(images_path, "*.jpeg"))
+
+def prepare_batch(images, network, channels=3):
+    width = network_width(network)
+    height = network_height(network)
+
+    darknet_images = []
+
+    for image in images:
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_rgb, (width, height),
+                                    interpolation=cv2.INTER_LINEAR)
+        custom_image = image_resized.transpose(2, 0, 1)
+        darknet_images.append(custom_image)
+    
+    batch_array = numpy.concatenate(darknet_images, axis=0)
+    batch_array = numpy.ascontiguousarray(batch_array.flat, dtype=np.float32)/255.0
+    darknet_images = batch_array.ctypes.data_as(POINTER(c_float))
+    return Image(width, height, channels, darknet_images)
+
+def image_detection(image_path, network, class_names, class_colors, thresh):
+    # Darknet does not accept numpy images
+    # Create one with image we reuse for each detect
+    width = network_width(network)
+    height = network_height(network)
+    darknet_image = make_image(width, height, 3)
+    
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height),
+                                interpolation=cv2.INTER_LINEAR)
+    
+    copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    detections = detect_image(network, class_names, darknet_image, thresh=thresh)
+    free_image(darknet_image)
+    image = draw_boxes(detections, image_resized, class_colors)
+
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
+
+
+def batch_detection(network, images, class_names, class_colors,
+                    thresh=0.25, hier_thresh=0.5, nms=0.45, batch_size=4):
+    image_height, image_width = check_batch_shape(images, batch_size)
+    darknet_images = prepare_batch(images, network)
+    batch_detections = network_predict_batch(network, darknet_images, batch_size, image_width,
+                                                image_height, thresh, hier_thresh, None, 0, 0)
+    
+    batch_predictions = []
+    for idx in range(batch_size):
+        num = batch_detections[idx].num
+        detections = batch_detections[idx].dets
+        if nms:
+            do_nms_obj(detections, num, len(class_names), nms)
+        predictions = remove_negatives(detections, class_names, num)
+        images[idx] = draw_boxes(predictions, images[idx], class_colors)
+        batch_predictions.append(predictions)
+
+    free_batch_detections(batch_detections, batch_size)
+
+    return images, batch_predictions
+
+def image_classification(image, network, class_names):
+    width = network_width(network)
+    height = network_height(network)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height),
+                                interpolation=cv2.INTER_LINEAR)
+    darknet_image = make_image(width, height, 3)
+    copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    detections = predict_image(network, darknet_image)
+    predictions = [(name, detections[idx]) for idx, name in enumerate(class_names)]
+    free_image(darknet_image)
+
+    return sorted(predictions, key=lambda x: -x[-1])
+
+def convert2relative(image, bbox):
+    """
+    convert to relative coordinates for the annotion
+    YOLO only uses this format
+    """
+    x, y, w, h = bbox
+    height, width , _ = image.shape
+
+    return x/width, y/height, w/width, h/height
+
+def save_annotions(name, image, detections, class_names):
+    """
+    files saved with image_name.txt and relative coordinates
+    """
+    file_name = os.path.splitext(name)[0] + ".txt"
+    with open(file_name, "w") as f:
+        for label, confidence, bbox in detections:
+            x, y, w, h = convert2relative(image, bbox)
+            label = class_names.index(label)
+            f.write("{} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(label, x, y, w, h, float(confidence)))
+
+def main():
+    random.seed(3)
+    network, class_names, class_colors = load_network(
+        config_file,
+        data_file,
+        weights,
+        batch_size
+    )
+
+    images = load_images(input_file)
+
+    index = 0
+    while True:
+        # loop asking for new image paths if no list is given
+        if input_file:
+            if index >= len(images):
+                break
+            image_name = images[index]
+        else:
+            image_name = input("Enter Image Path: ")
+
+        prev_time = time.time()
+        image, detections = image_detection(
+            image_name, network, class_names, class_colors, thresh
+        )
+        
+        print_detections(detections, False)
+        fps = int(1/(time.time() - prev_time))
+        print("FPS: {}".format(fps))
+        cv2.imshow('Inference', image)
+        if cv2.waitKey() & 0xFF == ord('q'):
+            break     
+        index += 1
+
+main()
 class Lexus_AI():
     bru = None
     
