e22f86cfb445c03223a407ac3124cce2c92deb2f
Tuna Cici <tunacici7@gmail.com>
2021-09-01 14:06:13 +0300

    Added fixed update rate support.

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ Author: Tuna Cici
 Created: 19/08/2021
 """
 
+import time
+
 from modules import logger
 from modules import config
+
+update_rate = 30.0 # update(s) per second
+print(f"{1/update_rate * 1000}ms per run")
 
 # entry point
 if __name__ == "__main__":
@@ -17,17 +22,28 @@ if __name__ == "__main__":
     main_logger.log_info("Starting Project Lexus...")
     main_logger.log_info(f"Current project directory: {config.PROJECT_DIR}")
 
+    curr_time = time.perf_counter()
+    prev_time = time.perf_counter()
+    elapsed = 0
     # our main loop
     while True:
         try:
             # TODO: Check if modules are alive
             # if they are down. initialize them.
-            if config.DEBUG_RUNNER == False:
+            if True == False:
                 print("initialize debugger screen.")
             else:
-                print("")
+                None
 
             # TODO: Design the program flow.
+            curr_time = time.perf_counter()
+            if (1 / update_rate) <= (curr_time - prev_time):
+                elapsed += 1
+                if update_rate <= elapsed:
+                    # a second has passed
+                    print("1 second passed")
+                    elapsed = 0
+                prev_time = time.perf_counter()
 
             # TODO: Update the modules.
             
