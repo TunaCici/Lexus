d30e923c6849ca46f83431c32e7c8797c9ce7799
Yasin Herken <57527891+Poyralljack@users.noreply.github.com>
2021-09-01 19:24:47 +0300

    voice adding

--- modules/voice_command.py
+++ modules/voice_command.py
@@ -1,4 +1,5 @@
 import os
+import queue
 from gtts import gTTS
 
 if __name__ == "modules." + os.path.basename(__file__)[:-3]:
@@ -14,7 +15,7 @@ else:
 # TODO:
 # 1. class name should be in capitals (ex. VoiceCommander)
 # 2. add a queue for voices to be played. (ex. files in the queue will be played one by one)
-# 3. add a request() function where you will get the text and the priorty add it to the queue
+# 3. add a request() function where you will get the text and the priority add it to the queue
 # 3. add a play() function where you will play the selected file/voice
 # 4. add an update() function where you check the queue for voices. 
 #    (ex. if there is something in the queue. call the play() fuction to play it and then remove it)
@@ -42,11 +43,13 @@ else:
 #                 I just want to give you some idea how they will play out.
 # Also the "voice_file" i wrote is a python dictionary. You can learn about it pretty easily.
 # I highly recommend that you learn it, it is very useful.
-
-class voice_commander:
+# A simple implementation of Priority Queue
+# using Queue.
+from collections import deque 
+from queue import Empty, Queue
+class VoiceCommander:
     # queue
-    queue = []
-
+    queue=deque()
     # logger
     voice_logger = logger.LexusLogger()
 
@@ -54,24 +57,18 @@ class voice_commander:
     is_playing = False
 
     # example voice file
-    vc_file = {
+    '''vc_file = {     
         "text:" : "Voice modules is initializing.",
         "path:" : config.PROJECT_DIR + "data/speech/merhaba.mp3",
         "priority": "low"
-    }
-
-    def __init__(self,text1='ağaç',distance=5):
+    }'''
+    def __init__(self,text1='ağaç',distance=10):
         
         #sistem dosyalarını daha rahat şekilde açmak için
-        import os
-        
+        import os     
         #Burada kullanacağımız 2 parametre bulunuyor, Dil ve Text
-        tts = gTTS(text=str(distance)+'metre sonra önünüze' +text1+'çıkacaktır', lang='tr')
         #Burada oluşturduğumuz ses dosyasını konuma merhaba.mp3 diye kaydediyoruz
-        tts.save("merhaba.mp3")
-
         #şimdi ise bu dosyayı açalım.
-        os.system("merhaba.mp3")
 
     def play(self, speech : dict):
         """
@@ -81,20 +78,31 @@ class voice_commander:
             self.voice_logger.log_warning("Voice file is empty.")
             return
         # how to get data from voice file
-        text = speech.get("text")
-        path = speech.get("path")
-        priority = speech.get("priority")
-
-        # TODO : do your magic
+        i = 0
+        for a in self.queue:
+            vc_file= self.queue[i]
+            text=vc_file.get("text")
+            os.system('espeak -v tr+f2 "{}"'.format(text))
+            i+=1
+            
+        
+       
+        
 
     def update(self):
         """
         checks the queue, if there is a voice file, plays it.
         """
-        if self.queue is None:
+        i = 0
+        for a in self.queue:
+            i+=1
+        if i == 0:
+            self.voice_logger.log_warning("Voice file is empty.")
             return
-        vc_file = self.queue.pop(0)
+        if self.queue is not None:      
+            vc_file= self.queue[0]
         self.play(vc_file)
+        
 
     def request(self, text : str, priority : str):
         """
@@ -102,18 +110,22 @@ class voice_commander:
         """
         if priority == "high":
             vc_file = {
-                "text:" : text,
-                "path:" : config.PROJECT_DIR + "data/speech/text0.mp3",
+                "text" : text,
+                "path" : config.PROJECT_DIR + "\\data\\speech",
                 "priority": priority
             }
             # TODO: add this vc_file at the front of the queue
-            ...
+            self.queue.appendleft(vc_file)
 
         else:
             vc_file = {
-                "text:" : text,
-                "path:" : config.PROJECT_DIR + "data/speech/text0.mp3",
+                "text" : text,
+                "path" : config.PROJECT_DIR + "\\data\\speech",
                 "priority": priority
             }
             # TODO: add this vc_file at the end of the queue
-            ...
+            self.queue.append(vc_file)
+            
+    def delete(self):
+        self.queue.clear()
+        print(self.queue)
