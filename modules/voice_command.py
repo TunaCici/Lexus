8d29a8ab594a3d91aa8eaae3b3788c318c92c10f
sirriyilmazkeskin@hotmail.com <astral73>
2021-09-18 17:53:32 +0300

    New

--- modules/voice_command.py
+++ modules/voice_command.py
@@ -1,5 +1,6 @@
 import os
 import queue
+
 if __name__ == "modules." + os.path.basename(__file__)[:-3]:
     # importing from outside the package
     from modules import config
@@ -8,6 +9,7 @@ else:
     # importing from main and inside the package
     import config
     import logger
+
 import os
 from gtts import gTTS
 from time import sleep
@@ -16,6 +18,7 @@ from collections import deque
 from queue import Empty, Queue
 import threading
 import pyglet
+import threading
 class VoiceCommander:
     # queue
     queue=deque()
@@ -24,39 +27,33 @@ class VoiceCommander:
 
     # a flah for cheking if a file is in play
     is_playing = False
-    def __init__(self):
-        import threading
-        import os
-        from gtts import gTTS
-        from time import sleep
-        import pyglet
+    def _init_(self):
+        
         tts1 = gTTS(text="Merhaba yol arkadaşım", lang='tr')
         filename1 =config.PROJECT_DIR +"\\data"+"\.mp3"
         tts1.save(filename1)
         music = pyglet.media.load(filename1, streaming=False)
         self.is_playing=True
         music.play()
-        sleep(music.duration)
+        self.islem(music.duration)
         os.remove(filename1)
         self.is_playing =False
-    def __del__(self):
+    def _del_(self):
         tts1 = gTTS(text="Bir sonraki yolculuğunda görüşmek üzere yol arkadaşım", lang='tr')
         filename1 =config.PROJECT_DIR +"\\data"+"\.mp3"
         tts1.save(filename1)
         music = pyglet.media.load(filename1, streaming=False)
         music.play()
         self.is_playing=True
-        sleep(music.duration)
+        self.islem(music.duration)
         os.remove(filename1)
         self.is_playing=False
-
-    def islem(second):
-        for i in range(int(second)+1):
-    
-    def islem(second):
-        for i in range(second):
-            sleep(1)
-            pass 
+    def islem(self,number):
+        if number < 0:
+            return False
+        self.is_playing= True
+        sleep(number)
+        self.is_playing= False
     
     def play(self, speech : dict):
         """
@@ -73,7 +70,7 @@ class VoiceCommander:
             music = pyglet.media.load(filename, streaming=False)
             self.is_playing= True
             music.play()
-            sleep(music.duration)
+            self.islem(music.duration)
             os.remove(filename)
             i+=1
             self.is_playing= False
@@ -116,11 +113,3 @@ class VoiceCommander:
             }
             # TODO: add this vc_file at the end of the queue
             self.queue.append(vc_file)
-a = VoiceCommander()
-a.request("naber","low")
-a.update()
-del a
-
-
-    
-        
